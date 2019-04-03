58fb70a752e038c816cb02fc709377edfda5c3e2
Włodek <starosto@interia.pl>
2019-04-03 11:01:36 +0200

    collections

--- src/com/javalesson/collections/collectionsClass/CollectionsRunner.java
+++ src/com/javalesson/collections/collectionsClass/CollectionsRunner.java
@@ -15,7 +15,6 @@ public class CollectionsRunner {
 //        System.out.println("List before sorting" + colors);
 //        Collections.sort(colors);
 //        System.out.println("List after sorting" + colors);
-
         List<Card> deckOfCards = new ArrayList<>();
         for (Card.Face face : Card.Face.values()) {
             for (Card.Suit suit : Card.Suit.values()) {
@@ -27,12 +26,35 @@ public class CollectionsRunner {
 //            System.out.printf("%-20s %s", deckOfCards.get(i), (i + 1) % 4 == 0 ? "\n" : " ");
 //        }
         Collections.shuffle(deckOfCards);
-        System.out.println("\n\nCards after shuffle");
-        for (int i = 0; i < deckOfCards.size(); i++) {
-            System.out.printf("%-20s %s", deckOfCards.get(i), (i + 1) % 4 == 0 ? "\n" : " ");
+        Collections.sort(deckOfCards);
+
+        Card card = new Card(Card.Suit.SPADES, Card.Face.Queen);
+        int i = Collections.binarySearch(deckOfCards, card);
+        if (i >= 0) {
+            System.out.println("Card was found at position " + i);
+        } else {
+            System.out.println("Card was not found");
         }
-        Collections.sort(deckOfCards, new CardComparator());
-        System.out.println("\nCards after sorting");
+
+        List<Card> cardList = new ArrayList<>(deckOfCards);
+        Collections.fill(cardList, card);
+        Collections.addAll(cardList, card, card, card);
+        Collections.copy(cardList, deckOfCards);
+        int frequency = Collections.frequency(cardList, card);
+        System.out.println("Frequency of " + card + " is " + frequency);
+
+
+        System.out.println("MIN : " + Collections.min(cardList));
+        System.out.println("MAX : " + Collections.max(cardList));
+  //      printOutput(cardList);
+//        System.out.println("\n\nCards after shuffle");
+//        printOutput(deckOfCards);
+//        Collections.sort(deckOfCards, new CardComparator());
+//        System.out.println("\nCards after sorting");
+        //     printOutput(deckOfCards);
+    }
+
+    private static void printOutput(List<Card> deckOfCards) {
         for (int i = 0; i < deckOfCards.size(); i++) {
             System.out.printf("%-20s %s", deckOfCards.get(i), (i + 1) % 4 == 0 ? "\n" : " ");
         }
@@ -76,12 +98,27 @@ public class CollectionsRunner {
         }
 
         @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (o == null || getClass() != o.getClass()) return false;
+            Card card = (Card) o;
+            return suit == card.suit &&
+                    face == card.face;
+        }
+
+        @Override
+        public int hashCode() {
+
+            return Objects.hash(suit, face);
+        }
+
+        @Override
         public String toString() {
             return face + " of " + suit;
         }
     }
 
-    public static class CardComparator implements Comparator<Card>{
+    public static class CardComparator implements Comparator<Card> {
         List<Card.Face> faces = Arrays.asList(Card.Face.values());
 
         @Override
@@ -91,7 +128,7 @@ public class CollectionsRunner {
             } else if (faces.indexOf(card1.getFace()) > faces.indexOf(card2.getFace())) {
                 return -1;
             } else if (faces.indexOf(card1.getFace()) == faces.indexOf(card2.getFace())) {
-                return 0;
+                return String.valueOf(card1.getSuit()).compareTo(String.valueOf(card2.getSuit()));
             }
             return 0;
         }
