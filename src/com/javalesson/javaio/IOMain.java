1055450db423cc01b26e42414641da13a028c5c7
Włodek <starosto@interia.pl>
2019-07-11 15:25:10 +0200

    formatter

--- src/com/javalesson/javaio/IOMain.java
+++ src/com/javalesson/javaio/IOMain.java
@@ -5,8 +5,7 @@ import com.javalesson.collections.map.treemap.SubjectGrade;
 import com.javalesson.collections.map.treemap.TreeMapRunner;
 
 import java.io.*;
-import java.util.NavigableMap;
-import java.util.Set;
+import java.util.*;
 
 public class IOMain {
 
@@ -14,21 +13,36 @@ public class IOMain {
 
     public static void main(String[] args) throws IOException {
         NavigableMap<AverageStudentGrade, Set<SubjectGrade>> grades = TreeMapRunner.createGrades();
-        readFile(grades);
+//        writeFile(grades);
+//        readFile();
 
-        readFile();
+        Formatter formatter = new Formatter("BankAccounts.txt");
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Please enter clientId, clientName, clientSurname, accountBalance");
+        int i = 0;
+        while (i < 3) {
+            try {
+                formatter.format("%d, %s, %s, %.2f%n", scanner.nextInt(), scanner.next(), scanner.next(), scanner.nextFloat());
+                i++;
+            } catch (InputMismatchException e) {
+                System.out.println("Input is incorrect. Please try again");
+                scanner.nextLine();
+            }
+        }
+        formatter.close();
     }
 
     private static void readFile() throws IOException {
         BufferedReader reader = new BufferedReader(new FileReader(FILE_NAME));
         String c;
-        while ((c = reader.readLine()) != null){
+        while ((c = reader.readLine()) != null) {
             System.out.println(c);
         }
     }
 
-    private static void readFile(NavigableMap<AverageStudentGrade, Set<SubjectGrade>> grades) throws IOException {
-        try (PrintWriter writer = new PrintWriter(new FileWriter(FILE_NAME))) {
+    private static void writeFile(NavigableMap<AverageStudentGrade, Set<SubjectGrade>> grades) throws IOException {
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(FILE_NAME))) {
             for (AverageStudentGrade gradeKey : grades.keySet()) {
                 writer.write("________________________________\n");
                 writer.write("Student: " + gradeKey.getName() + " Average grade: " + gradeKey.getAverageGrade() + "\n");
